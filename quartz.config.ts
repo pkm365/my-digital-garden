f2bfc23ae19e4d762ec9c8ca833ea05dc6a30313
kejiafeng <jarry.feng@qq.com>
2025-06-11 13:59:52 +0800

    feat: 更新样式文件，优化左侧边栏卡片化效果

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jarry's Digital Garden",
+    pageTitle: "🌱Jarry's Digital Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
