f0e24d3118257abf63898f3bcd89291a2e1a982e
kejiafeng <jarry.feng@qq.com>
2025-06-18 17:11:58 +0800

    add recent notes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -64,6 +64,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode(), shrink: false },
       ],
     }),
+    Component.RecentNotes(),
     Component.Explorer(),
   ],
   right: [
